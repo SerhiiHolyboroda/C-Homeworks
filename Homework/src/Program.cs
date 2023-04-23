1f94ea75f75ac228fe0eddc7f254c4925e87cea3
Serhii <nosferatugamevampire@gmail.com>
2023-04-23 21:03:11 +0300

    Adding files for task 2

--- Homework/src/Program.cs
+++ Homework/src/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 namespace Homework.src
 {
@@ -10,6 +14,39 @@ namespace Homework.src
     {
         static void Main(string[] args)
         {
+
+            // task 1
+            Console.WriteLine("Please, input text to extract numbers");
+            string input = Convert.ToString(Console.ReadLine());
+            MatchCollection matches = Regex.Matches(input, @"[-+]?\d+(\.\d+)?");
+           Console.WriteLine("Sum of all numbers from string = "+  MathOperations.NumberSumFromString(matches));
+           Console.WriteLine("Maximum number froms string = " + MathOperations.MaximumNumber(matches));
+
+            // task 2
+            Console.WriteLine("Please, input text to extract index of largest number");
+            string StringOfNumbers = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Idnex of largest number = " +  MathOperations.GetIndexOfMaximumNumber(StringOfNumbers));
+            // task 3
+            Random randomnumber = new Random();
+            int[] pages = new int[100];
+
+            for (int i = 0; i < pages.Length; i++)
+            {
+                pages[i] = randomnumber.Next(100);
+            }
+            Console.WriteLine("Number of pages of largest book: " + MathOperations.GetNumberOfPages(pages));
+
+            // task 4
+            Random random = new Random();
+            int[] randomCarsSpeeds = new int[40];
+
+            for (int i = 0; i < randomCarsSpeeds.Length; i++)
+            {
+                randomCarsSpeeds[i] = random.Next(40);
+            }
+
+            Console.WriteLine(" Indexes of highest  number in Array are: " +  MathOperations.GetIndexOfFastestCar(randomCarsSpeeds));
+
 
         }
                 }
